ffa79145a25b168c75e8f1ae3666da3a459aa32d
LH <l.holota@outlook.com>
2018-08-10 09:32:03 +0200

    Positional arguments support

--- tests/LH.CommandLine.UnitTests/OptionsParser/WhenParsingPositionalArguments.cs
+++ tests/LH.CommandLine.UnitTests/OptionsParser/WhenParsingPositionalArguments.cs
@@ -1,14 +1,29 @@
-﻿namespace LH.CommandLine.UnitTests.OptionsParser
+﻿using LH.CommandLine.Options;
+using Xunit;
+
+namespace LH.CommandLine.UnitTests.OptionsParser
 {
     public class WhenParsingPositionalArguments
     {
+        [Theory]
+        [InlineData(new[] { "some-value", "529" }, "some-value", 529)]
+        [InlineData(new[] { "some-value" }, "some-value", 0)]
+        public void ShouldParsePositionalArguments(string[] args, string expectedValue1, int expectedValue2)
+        {
+            var parser = new OptionsParser<OptionsWithPositional>();
+            var options = parser.Parse(args);
+
+            Assert.Equal(expectedValue1, options.Value1);
+            Assert.Equal(expectedValue2, options.Value2);
+        }
+
         private class OptionsWithPositional
         {
             [Argument(0)]
             public string Value1 { get; set; }
 
             [Argument(1)]
-            public string Value2 { get; set; }
+            public int Value2 { get; set; }
         }
     }
 }
