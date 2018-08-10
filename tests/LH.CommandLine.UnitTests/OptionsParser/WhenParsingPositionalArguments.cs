e44d336c3e3314ef76200720e363fe123e093469
LH <l.holota@outlook.com>
2018-08-10 08:58:01 +0200

    AppVeyor build fixed

--- tests/LH.CommandLine.UnitTests/OptionsParser/WhenParsingPositionalArguments.cs
+++ tests/LH.CommandLine.UnitTests/OptionsParser/WhenParsingPositionalArguments.cs
@@ -2,6 +2,13 @@
 {
     public class WhenParsingPositionalArguments
     {
+        private class OptionsWithPositional
+        {
+            [Argument(0)]
+            public string Value1 { get; set; }
 
+            [Argument(1)]
+            public string Value2 { get; set; }
+        }
     }
 }
