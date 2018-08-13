00bead66c857c461b25e854b90c436bb6c067d87
LH <l.holota@outlook.com>
2018-08-13 09:13:17 +0200

    Added tests for collections

---
+++ tests/LH.CommandLine.UnitTests/OptionsParser/WhenParsingCollections.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+
+namespace LH.CommandLine.UnitTests.OptionsParser
+{
+    public class WhenParsingCollections
+    {
+        /*
+         * Collection with spaces cannot be positional!
+         * -> Separator
+         * -> asda,asd,asd,asda,sd,asdad
+         * -> 1|2|3.5|3,5
+         * -> 1,2,3,4,5
+         * "asasd as adasd,asdasd sda asd a,asdasdas sd asda sd"
+         * "asdasd", "asdasd", "asdasd", "asdasdasd", "asdasd" -> quotes are lost when dotnet parses args :/
+         */
+
+        [Fact]
+        public void ShouldParseIEnumerableOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseArrayOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseReadOnlyCollectionOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseCollectionOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseReadOnlyListOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseListOfIntegers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldParseCollectionWithCustomParser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldPassWhenCollectionHasNoItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldThrowIfCollectionValidationFails()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldNotIncludeNextNamedOptionIntoCollection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
